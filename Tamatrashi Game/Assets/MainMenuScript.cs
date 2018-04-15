dea0f2eeee8f384cfbee0e1bfc00636636cff321
julngo <julian.t.ngo@live.com>
2018-04-15 15:50:07 -0700

    Made a working options Menu

---
+++ Tamatrashi Game/Assets/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActive().buildIndex +1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You've Quit the Game");
+        Application.Quit();
+    }
+}
---
+++ Tamatrashi Game/Assets/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScr)
+    {
+        Screen.fullScreen = isFullScr;
+    }
+}
