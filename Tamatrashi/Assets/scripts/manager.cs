f634393618b28feed9f6417f9cdc7ff5915acbb5
Su <rice74@uw.edu>
2018-05-06 16:31:06 -0700

    added stats, click pet to increase happiness

---
+++ Tamatrashi/Assets/scripts/manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour {
+
+	public GameObject sHappiness;
+	public GameObject sHunger;
+	public GameObject sHygiene;
+	public GameObject sHealth;
+	public GameObject pet;
+
+	// Update is called once per frame
+	void Update () {
+		//monitor and update the stats
+		sHappiness.GetComponent<Text> ().text = "" + pet.GetComponent<pet> ().happiness;
+		sHunger.GetComponent<Text> ().text = "" + pet.GetComponent<pet> ().hunger;
+		sHygiene.GetComponent<Text> ().text = "" + pet.GetComponent<pet> ().hygiene;
+		sHealth.GetComponent<Text> ().text = "" + pet.GetComponent<pet> ().health;
+
+	}
+}
---
+++ Tamatrashi/Assets/scripts/pet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pet : MonoBehaviour {
+
+	public int health;
+	public int hygiene;
+	public int hunger;
+	public int happiness;
+
+	// Use this for initialization
+	void Start () {
+		//init values
+		health = 7;
+		hygiene = 7;
+		hunger = 7;
+		happiness = 7;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) { //if pet was clicked
+			updateStatus("happiness", 1);
+		}
+	}
+
+	void updateStatus(string type, int val){
+		//checks and update the corresponding stat
+		switch (type){
+		case "health":
+			if (health + val > 10) {
+				return;
+			} else {
+				health = health + val;
+			}
+			break;
+		case "hygiene":
+			if (hygiene + val > 10) {
+				return;
+			} else {
+				hygiene = hygiene + val;
+			}
+			break;
+		case "hunger":
+			if (hunger + val > 10) {
+				return;
+			} else {
+				hunger = hunger + val;
+			}
+			break;
+		case "happiness":
+			if (happiness + val > 10) {
+				return;
+			} else {
+				happiness = happiness + val;
+			}
+			break;
+		default:
+			return;
+		}
+	}
+}
