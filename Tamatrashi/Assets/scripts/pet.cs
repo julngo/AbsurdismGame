e58a42f3d995953ff8d56642c81bb4eda2004740
Su <rice74@uw.edu>
2018-05-07 15:53:00 -0700

    added name change, animation states switching

--- Tamatrashi/Assets/scripts/pet.cs
+++ Tamatrashi/Assets/scripts/pet.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pet : MonoBehaviour {
-
+	public Text petName;
 	public int health;
 	public int hygiene;
 	public int hunger;
 	public int happiness;
+	Animator animator;
+	private bool clickedBarf = false;
 
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator> ();
 		//init values
+		string name = PlayerPrefs.GetString("petName");
+		petName.text = name;
 		health = 7;
 		hygiene = 7;
 		hunger = 7;
@@ -20,8 +26,11 @@ public class pet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonUp (0)) { //if pet was clicked
-			updateStatus("happiness", 1);
+		if (Input.GetMouseButtonUp (0) && !clickedBarf) { //if pet was clicked
+			animator.Play("barf");
+			updateStatus("happiness", -1);
+			updateStatus("hygiene", -1);
+			clickedBarf = true;
 		}
 	}
 
@@ -31,12 +40,16 @@ public class pet : MonoBehaviour {
 		case "health":
 			if (health + val > 10) {
 				return;
-			} else {
+			} else if (health + val < 0) {
+				return;
+			}else {
 				health = health + val;
 			}
 			break;
 		case "hygiene":
 			if (hygiene + val > 10) {
+				return;
+			} else if (hygiene + val < 0) {
 				return;
 			} else {
 				hygiene = hygiene + val;
@@ -45,12 +58,16 @@ public class pet : MonoBehaviour {
 		case "hunger":
 			if (hunger + val > 10) {
 				return;
+			} else if (hunger + val < 0) {
+				return;
 			} else {
 				hunger = hunger + val;
 			}
 			break;
 		case "happiness":
 			if (happiness + val > 10) {
+				return;
+			} else if (happiness + val < 0) {
 				return;
 			} else {
 				happiness = happiness + val;
--- Tamatrashi/Assets/scripts/start.cs
+++ Tamatrashi/Assets/scripts/start.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class start : MonoBehaviour {
 
+	public GameObject menu;
+	public GameObject title;
+
 	public void startGame(){
-		SceneManager.LoadScene ("game");
+		menu.SetActive (true);
+		title.SetActive (false);
 	}
 }
